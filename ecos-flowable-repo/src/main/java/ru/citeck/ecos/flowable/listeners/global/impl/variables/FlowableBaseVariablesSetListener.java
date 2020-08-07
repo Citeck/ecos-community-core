05d10a0d711450b6f7570f8fa3da4a746accaa58
Roman Makarskiy <Romanchabest55@gmail.com>
2020-08-07 13:28:46 +0300

    Set webUrl, shareUrl to flowable process by default

--- ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/listeners/global/impl/variables/FlowableBaseVariablesSetListener.java
+++ ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/listeners/global/impl/variables/FlowableBaseVariablesSetListener.java
@@ -7,6 +7,7 @@ import org.alfresco.util.UrlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.citeck.ecos.flowable.variable.FlowableScriptNode;
+import ru.citeck.ecos.utils.UrlUtils;
 
 /**
  * @author Roman Makarskiy
@@ -19,12 +20,15 @@ public class FlowableBaseVariablesSetListener extends AbstractFlowableSaveToExec
 
     private static final String VAR_DOCUMENT = "document";
     private static final String VAR_SHARE_URL = "shareUrl";
+    private static final String VAR_WEB_URL = "webUrl";
 
     private final SysAdminParams sysAdminParams;
+    private final UrlUtils urlUtils;
 
     @Autowired
-    public FlowableBaseVariablesSetListener(SysAdminParams sysAdminParams) {
+    public FlowableBaseVariablesSetListener(SysAdminParams sysAdminParams, UrlUtils urlUtils) {
         this.sysAdminParams = sysAdminParams;
+        this.urlUtils = urlUtils;
     }
 
     @Override
@@ -36,6 +40,7 @@ public class FlowableBaseVariablesSetListener extends AbstractFlowableSaveToExec
     public void saveToExecution(String executionId, NodeRef document) {
         setDocumentVariable(executionId, document);
         setShareUrlVariable(executionId);
+        setWebUrlVariable(executionId);
     }
 
     private void setDocumentVariable(String executionId, NodeRef document) {
@@ -50,5 +55,9 @@ public class FlowableBaseVariablesSetListener extends AbstractFlowableSaveToExec
     private void setShareUrlVariable(String executionId) {
         String shareUrl = UrlUtil.getShareUrl(sysAdminParams);
         setVariable(executionId, VAR_SHARE_URL, shareUrl);
+    }
+
+    private void setWebUrlVariable(String executionId) {
+        setVariable(executionId, VAR_WEB_URL, urlUtils.getWebUrl());
     }
 }
