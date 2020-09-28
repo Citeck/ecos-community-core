9b147c88c7255dfee4bb91271e1ed32285480e59
Pavel Simonov <pavel.simonov@citeck.ru>
2020-09-28 17:24:37 +0700

    ECOSCOM-3956 - Attributes Permissions. Don't check viewable attrs

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/security/EcosPermissionService.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/security/EcosPermissionService.java
@@ -1,12 +1,14 @@
 package ru.citeck.ecos.security;
 
+import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import ru.citeck.ecos.node.AlfNodeInfo;
+import ru.citeck.ecos.node.AlfNodeInfoImpl;
 
 import java.util.HashSet;
 import java.util.List;
@@ -19,14 +21,31 @@ public class EcosPermissionService {
 
     private static final String EDIT_MODE = "edit";
 
+    /**
+     * @deprecated use EcosPermissionComponent instead
+     */
+    @Deprecated
     private AttributesPermissionServiceResolver attsPermServiceResolver;
+
+    private EcosPermissionComponent ecosPermissionComponent;
+
     private NamespaceService namespaceService;
+    private ServiceRegistry serviceRegistry;
 
     private Set<QName> protectedAttributes = new HashSet<>();
 
     public boolean isAttributeProtected(NodeRef nodeRef, String attributeName) {
 
         if (nodeRef == null || StringUtils.isBlank(attributeName)) {
+            return false;
+        }
+
+        return isAttProtected(new AlfNodeInfoImpl(nodeRef, serviceRegistry), attributeName);
+    }
+
+    public boolean isAttProtected(AlfNodeInfo node, String attributeName) {
+
+        if (node == null || StringUtils.isBlank(attributeName)) {
             return false;
         }
 
@@ -42,16 +61,41 @@ public class EcosPermissionService {
         if (protectedAttributes.contains(attQName)) {
             return true;
         }
+
+        if (ecosPermissionComponent != null) {
+            return ecosPermissionComponent.isAttProtected(node, attributeName);
+        }
+
         if (attsPermServiceResolver == null) {
             return false;
         }
-        AttributesPermissionService attrsPermissionService = attsPermServiceResolver.resolve(nodeRef);
-        return attrsPermissionService == null ? false : !attrsPermissionService.isFieldEditable(attQName, nodeRef, EDIT_MODE);
+
+        AttributesPermissionService attrsPermissionService = attsPermServiceResolver.resolve(node.getNodeRef());
+
+        if (attrsPermissionService == null) {
+            return false;
+        }
+
+        return !attrsPermissionService.isFieldEditable(attQName, node.getNodeRef(), EDIT_MODE);
+    }
+
+    public boolean isAttVisible(AlfNodeInfo info, String attributeName) {
+
+        if (info == null || StringUtils.isBlank(attributeName) || ecosPermissionComponent == null) {
+            return true;
+        }
+
+        return ecosPermissionComponent.isAttVisible(info, attributeName);
     }
 
     @Autowired(required = false)
     public void setAttsPermServiceResolver(AttributesPermissionServiceResolver attsPermServiceResolver) {
         this.attsPermServiceResolver = attsPermServiceResolver;
+    }
+
+    @Autowired(required = false)
+    public void setEcosPermissionComponent(EcosPermissionComponent ecosPermissionComponent) {
+        this.ecosPermissionComponent = ecosPermissionComponent;
     }
 
     @Autowired
@@ -67,5 +111,10 @@ public class EcosPermissionService {
         Set<QName> newAtts = new HashSet<>(this.protectedAttributes);
         newAtts.addAll(protectedAttributes);
         this.protectedAttributes = newAtts;
+    }
+
+    @Autowired
+    public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+        this.serviceRegistry = serviceRegistry;
     }
 }
