ab2fcfa69d3b0e590ff7bef93df2aec65e94c626
Zaytsev Aleksey <aleksey.zaytsev@citeck.ru>
2020-11-13 13:56:54 +0700

    ECOSCOM-4024 - Add statuses info on counterparty dashboard and contract form PR changes

--- idocs-repo/src/main/resources/alfresco/module/idocs-repo/bootstrap/scripts/contractor-statuses.js
+++ idocs-repo/src/main/resources/alfresco/module/idocs-repo/bootstrap/scripts/contractor-statuses.js
@@ -1,34 +1,21 @@
-var statuses = [
-    {name: 'idocs-contractor-green',        title: {'en': 'Green',      'ru': '\u0417\u0435\u043b\u0435\u043d\u044b\u0439'}},
-    {name: 'idocs-contractor-yellow',       title: {'en': 'Yellow',     'ru': '\u0416\u0451\u043b\u0442\u044b\u0439'}},
-    {name: 'idocs-contractor-red',          title: {'en': 'Red',        'ru': '\u041a\u0440\u0430\u0441\u043d\u044b\u0439'}}
+<import resource="classpath:alfresco/module/idocs-repo/scripts/import-nodes.lib.js">
+
+var type = '{http://www.citeck.ru/model/icase/1.0}caseStatus';
+var destination = 'app:company_home/app:dictionary/cm:dataLists/cm:case-status';
+
+var data = [
+    {
+        type: type,
+        props: {'cm:name': 'idocs-contractor-green', 'cm:title': {'en': 'Green', 'ru': '\u0417\u0435\u043b\u0435\u043d\u044b\u0439'}}
+    },
+    {
+        type: type,
+        props: {'cm:name': 'idocs-contractor-yellow', 'cm:title': {'en': 'Yellow', 'ru': '\u0416\u0451\u043b\u0442\u044b\u0439'}}
+    },
+    {
+        type: type,
+        props: {'cm:name': 'idocs-contractor-red', 'cm:title': {'en': 'Red', 'ru': '\u041a\u0440\u0430\u0441\u043d\u044b\u0439'}}
+    }
 ];
 
-function main(statuses) {
-
-    var statusNode,
-        repositoryHelper = services.get('repositoryHelper'),
-        companyhome = search.findNode(repositoryHelper.getCompanyHome()),
-        root = (companyhome.childrenByXPath("app:dictionary/cm:dataLists/cm:case-status") || [])[0];
-
-    if (!root) {
-        logger.warn("[case-statuses.js] Case statuses root not found!");
-        return;
-    }
-
-    for (var i in statuses) {
-        var status = statuses[i];
-        statusNode = root.childByNamePath(status.name);
-        if (statusNode == null) {
-            statusNode = root.createNode(status.name, '{http://www.citeck.ru/model/icase/1.0}caseStatus');
-        }
-        utils.setLocale("en_US");
-        statusNode.properties['cm:title'] = status.title.en;
-        statusNode.save();
-        utils.setLocale("ru_RU");
-        statusNode.properties['cm:title'] = status.title.ru;
-        statusNode.save();
-    }
-}
-
-main(statuses);
+importNodes(destination, data);
