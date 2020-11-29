aa1a1840915e9ff9f7582f6052476b251f1900af
Baitazar Ishmukhametov <baitazar.ishmukhametov@citeck.ru>
2020-11-29 13:31:12 +0300

    ECOSCOM-3959 Release task if task completion resulted in exception

--- idocs-repo/src/main/java/ru/citeck/ecos/workflow/tasks/EcosTaskService.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/workflow/tasks/EcosTaskService.java
@@ -90,7 +90,8 @@ public class EcosTaskService {
             }
         }
 
-        if (StringUtils.isBlank(assignee)) {
+        boolean isAssigneeEmpty = StringUtils.isBlank(assignee);
+        if (isAssigneeEmpty) {
             ownerService.changeOwner(taskId, OwnerAction.CLAIM, user);
         }
 
@@ -106,6 +107,9 @@ public class EcosTaskService {
                 return null;
             });
         } catch (RuntimeException exception) {
+            if (isAssigneeEmpty) {
+                ownerService.changeOwner(taskId, OwnerAction.RELEASE, user);
+            }
             unwrapJsExceptionAndThrow(exception);
         }
     }
