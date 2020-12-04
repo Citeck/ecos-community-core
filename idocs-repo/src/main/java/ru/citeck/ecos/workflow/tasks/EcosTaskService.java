3c5753f3a2203236aef28dd3b7739032a24718c3
Baitazar Ishmukhametov <baitazar.ishmukhametov@citeck.ru>
2020-12-04 17:48:47 +0300

    ECOSCOM-3959 add nested exception as supressed

--- idocs-repo/src/main/java/ru/citeck/ecos/workflow/tasks/EcosTaskService.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/workflow/tasks/EcosTaskService.java
@@ -111,7 +111,7 @@ public class EcosTaskService {
                 try {
                     ownerService.changeOwner(taskId, OwnerAction.RELEASE, user);
                 } catch (Exception changeOwnerException) {
-                    throw changeOwnerException;
+                    exception.addSuppressed(changeOwnerException);
                 }
             }
             unwrapJsExceptionAndThrow(exception);
