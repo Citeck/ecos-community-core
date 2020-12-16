06fe3b5d64114092824f4e737243fdd01f233e81
Pavel Simonov <pavel.simonov@citeck.ru>
2020-12-16 21:41:56 +0700

    ECOSCOM-4042 - Migrate case roles settings to ECOS type

--- idocs-repo/src/main/java/ru/citeck/ecos/records/notification/NotificationCommandServiceJS.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/records/notification/NotificationCommandServiceJS.java
@@ -1,17 +1,63 @@
 package ru.citeck.ecos.records.notification;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.citeck.ecos.commons.data.DataValue;
+import ru.citeck.ecos.commons.json.Json;
 import ru.citeck.ecos.notifications.lib.Notification;
 import ru.citeck.ecos.notifications.lib.NotificationType;
 import ru.citeck.ecos.notifications.lib.service.NotificationService;
 import ru.citeck.ecos.records2.RecordRef;
 import ru.citeck.ecos.utils.AlfrescoScopableProcessorExtension;
+import ru.citeck.ecos.utils.JsUtils;
+import ru.citeck.ecos.utils.UrlUtils;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class NotificationCommandServiceJS extends AlfrescoScopableProcessorExtension {
 
     private NotificationService notificationService;
+    private UrlUtils urlUtils;
+    private JsUtils jsUtils;
+
+    public void send(Object data) {
+
+        Object notificationObj = jsUtils.toJava(data);
+        DataValue notificationValue = Json.getMapper().convert(notificationObj, DataValue.class);
+
+        if (notificationValue == null) {
+            throw new IllegalArgumentException("Incorrect notification: " + data);
+        }
+
+        DataValue record = notificationValue.get("record");
+        Object notificationRecord;
+        if (record.isTextual()) {
+            notificationRecord = RecordRef.valueOf(record.asText());
+        } else {
+            notificationRecord = record;
+        }
+
+        Notification notification = new Notification.Builder()
+            .record(notificationRecord)
+            .templateRef(RecordRef.valueOf(notificationValue.get("templateRef").asText()))
+            .notificationType(NotificationType.valueOf(notificationValue.get("notificationType").asText()))
+            .recipients(notificationValue.get("recipients").asStrList())
+            .from(asStringOrNull(notificationValue.get("from")))
+            .lang(asStringOrNull(notificationValue.get("lang")))
+            .additionalMeta(
+                getAdditionalMeta(notificationValue.get("additionalMeta").asMap(String.class, Object.class))
+            ).build();
+
+        notificationService.send(notification);
+    }
+
+    private String asStringOrNull(DataValue value) {
+        if (value.isTextual()) {
+            return value.asText();
+        }
+        return null;
+    }
 
     public void send(String record, String templateRef, String type, List<String> recipients, String from, String lang,
                      Map<String, Object> additionalMeta) {
@@ -23,14 +69,37 @@ public class NotificationCommandServiceJS extends AlfrescoScopableProcessorExten
             .recipients(recipients)
             .from(from)
             .lang(lang)
-            .additionalMeta(additionalMeta)
+            .additionalMeta(getAdditionalMeta(additionalMeta))
             .build();
 
         notificationService.send(notification);
+    }
 
+    private Map<String, Object> getAdditionalMeta(Map<String, Object> baseMeta) {
+
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("webUrl", urlUtils.getWebUrl());
+        result.put("shareUrl", urlUtils.getShareUrl());
+
+        if (baseMeta != null) {
+            result.putAll(baseMeta);
+        }
+
+        return result;
     }
 
     public void setNotificationService(NotificationService notificationService) {
         this.notificationService = notificationService;
+    }
+
+    @Autowired
+    public void setUrlUtils(UrlUtils urlUtils) {
+        this.urlUtils = urlUtils;
+    }
+
+    @Autowired
+    public void setJsUtils(JsUtils jsUtils) {
+        this.jsUtils = jsUtils;
     }
 }
