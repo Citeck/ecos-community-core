b4a871874a8722c421431bb711455558c5079ca7
Zaytsev Aleksey <aleksey.zaytsev@citeck.ru>
2020-12-21 18:06:16 +0700

    ECOSCOM-4103 - Ecos statuses. Rework TaskDataListenerUtils.fillDocumentData() for support ecos statuses Reworked

--- idocs-repo/src/main/java/ru/citeck/ecos/workflow/listeners/TaskDataListenerUtils.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/workflow/listeners/TaskDataListenerUtils.java
@@ -1,54 +1,88 @@
 package ru.citeck.ecos.workflow.listeners;
 
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.node.MLPropertyInterceptor;
+import org.alfresco.service.cmr.repository.MLText;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.citeck.ecos.icase.CaseStatusService;
 import ru.citeck.ecos.model.HistoryModel;
+import ru.citeck.ecos.model.lib.status.dto.StatusDef;
 import ru.citeck.ecos.records2.RecordRef;
-import ru.citeck.ecos.records2.RecordsService;
-import ru.citeck.ecos.workflow.listeners.model.TaskListenerDocumentInfo;
+import ru.citeck.ecos.records3.RecordsService;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class TaskDataListenerUtils {
 
     private static final String TITLE_DELIMITER = "|";
+    private static final Locale LOCAL_RU = Locale.forLanguageTag("ru");
 
     private final RecordsService recordsService;
     private final NodeService nodeService;
+    private final CaseStatusService caseStatusService;
 
     @Autowired
-    public TaskDataListenerUtils(RecordsService recordsService, NodeService nodeService) {
+    public TaskDataListenerUtils(RecordsService recordsService, NodeService nodeService,
+                                 CaseStatusService caseStatusService) {
         this.recordsService = recordsService;
         this.nodeService = nodeService;
+        this.caseStatusService = caseStatusService;
     }
 
     public void fillDocumentData(NodeRef documentRef, Map<QName, Serializable> eventProperties) {
-        if (documentRef != null && nodeService.exists(documentRef)) {
-            TaskListenerDocumentInfo documentMeta = recordsService.getMeta(
-                RecordRef.create("", documentRef.toString()), TaskListenerDocumentInfo.class);
-            eventProperties.put(HistoryModel.PROP_DOC_TYPE, documentMeta.getDocumentType());
-            eventProperties.put(HistoryModel.PROP_DOC_STATUS_NAME, documentMeta.getStatusName());
-            eventProperties.put(HistoryModel.PROP_DOC_STATUS_TITLE, getTitleData(documentMeta));
+        if (documentRef == null || !nodeService.exists(documentRef)) {
+            return;
+        }
 
+        String type = recordsService.getAtt(RecordRef.valueOf(documentRef.toString()), "TYPE").asText();
+        eventProperties.put(HistoryModel.PROP_DOC_TYPE, type);
+
+        fillStatusData(documentRef, eventProperties);
+    }
+
+    private void fillStatusData(NodeRef documentRef, Map<QName, Serializable> eventProperties) {
+        NodeRef statusRef = caseStatusService.getStatusRef(documentRef);
+        if (caseStatusService.isAlfRef(statusRef)) {
+            fillAlfrescoStatusData(eventProperties, statusRef);
+        } else {
+            StatusDef statusDef = caseStatusService.getStatusDef(documentRef, statusRef.getId());
+            fillEcosStatusData(eventProperties, statusDef);
         }
     }
 
-    //TODO: rewrite to normal format?
-    private static String getTitleData(TaskListenerDocumentInfo documentMeta) {
-        List<String> titles = new ArrayList<>();
-        titles.add(documentMeta.getStatusTitleEn());
-        titles.add(documentMeta.getStatusTitleRu());
+    private void fillEcosStatusData(Map<QName, Serializable> eventProperties, StatusDef statusDef) {
+        eventProperties.put(HistoryModel.PROP_DOC_STATUS_NAME, statusDef.getId());
+        ru.citeck.ecos.commons.data.MLText name = statusDef.getName();
+        String titleStr = mlTextToLine(name.get(Locale.ENGLISH), name.get(LOCAL_RU));
+        eventProperties.put(HistoryModel.PROP_DOC_STATUS_TITLE, titleStr);
+    }
 
-        return titles.stream()
+    private void fillAlfrescoStatusData(Map<QName, Serializable> eventProperties, NodeRef statusRef) {
+        MLPropertyInterceptor.setMLAware(true);
+        MLText title = (MLText) nodeService.getProperty(statusRef, ContentModel.PROP_TITLE);
+        MLPropertyInterceptor.setMLAware(false);
+        String name = (String) nodeService.getProperty(statusRef, ContentModel.PROP_NAME);
+
+        eventProperties.put(HistoryModel.PROP_DOC_STATUS_NAME, name);
+
+        String titleStr = null;
+        if (title != null) {
+            titleStr = mlTextToLine(title.get(Locale.ENGLISH), title.get(LOCAL_RU));
+        }
+        eventProperties.put(HistoryModel.PROP_DOC_STATUS_TITLE, titleStr);
+    }
+
+    private String mlTextToLine(String... text) {
+        return Stream.of(text)
             .filter(StringUtils::isNoneBlank)
             .collect(Collectors.joining(TITLE_DELIMITER));
     }
