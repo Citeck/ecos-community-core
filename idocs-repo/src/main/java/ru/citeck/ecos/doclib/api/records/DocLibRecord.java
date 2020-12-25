1de9eefd4c4ea47fb75098ea79ca8e55479683eb
Pavel Simonov <pavel.simonov@citeck.ru>
2020-12-25 11:21:26 +0700

    ECOSCOM-4061 - [DocLib] Develop DAO for documents library

--- idocs-repo/src/main/java/ru/citeck/ecos/doclib/api/records/DocLibRecord.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/doclib/api/records/DocLibRecord.java
@@ -1,14 +1,22 @@
 package ru.citeck.ecos.doclib.api.records;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.alfresco.service.cmr.repository.ContentData;
+import org.alfresco.service.cmr.repository.NodeRef;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.json.JSONArray;
 import ru.citeck.ecos.doclib.service.DocLibChildrenQuery;
 import ru.citeck.ecos.doclib.service.DocLibNodeInfo;
 import ru.citeck.ecos.doclib.service.DocLibNodeType;
 import ru.citeck.ecos.doclib.service.DocLibService;
 import ru.citeck.ecos.records.source.PeopleRecordsDao;
+import ru.citeck.ecos.records.source.alf.file.FileRepresentation;
 import ru.citeck.ecos.records2.RecordConstants;
 import ru.citeck.ecos.records2.RecordRef;
 import ru.citeck.ecos.records3.record.op.atts.service.schema.annotation.AttName;
+import ru.citeck.ecos.records3.record.op.atts.service.value.AttValue;
 
 import java.util.Date;
 import java.util.List;
@@ -83,9 +91,24 @@ public class DocLibRecord {
         return getDocLibNodeInfo().getDisplayName();
     }
 
-    @AttName(RecordConstants.ATT_MODIFIED)
-    public Date getModified() {
-        return getDocLibNodeInfo().getModified();
+    @AttName(RecordConstants.ATT_CONTENT)
+    public Object getContent() {
+
+        DocLibNodeInfo nodeInfo = getDocLibNodeInfo();
+
+        String recordRefId = nodeInfo.getRecordRef().getId();
+        if (!recordRefId.contains("$") || recordRefId.charAt(recordRefId.length() - 1) == '$') {
+            return null;
+        }
+        recordRefId = recordRefId.substring(recordRefId.indexOf('$') + 1);
+        if (!NodeRef.isNodeRef(recordRefId)) {
+            return null;
+        }
+        ContentData contentData = nodeInfo.getContent().get();
+        if (contentData == null) {
+            return null;
+        }
+        return new RecordContentData(new NodeRef(recordRefId), contentData);
     }
 
     @AttName(RecordConstants.ATT_CREATED)
@@ -113,5 +136,24 @@ public class DocLibRecord {
     @AttName("?json")
     public DocLibNodeInfo getJson() {
         return getDocLibNodeInfo();
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    public class RecordContentData implements AttValue {
+
+        private final NodeRef nodeRef;
+        private final ContentData data;
+
+        @Nullable
+        @Override
+        public Object getAs(String type) {
+
+            if ("content-data".equals(type)) {
+                JSONArray array = FileRepresentation.formContentData(data, docLibRecords.getNodeService(), nodeRef);
+                return array.toString();
+            }
+            return null;
+        }
     }
 }
--- idocs-repo/src/main/java/ru/citeck/ecos/doclib/service/DocLibNodeInfo.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/doclib/service/DocLibNodeInfo.java
@@ -2,13 +2,17 @@ package ru.citeck.ecos.doclib.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.alfresco.service.cmr.repository.ContentData;
+import org.jetbrains.annotations.NotNull;
 import ru.citeck.ecos.records2.RecordRef;
 
 import java.util.Date;
+import java.util.function.Supplier;
 
 @Data
 @AllArgsConstructor
 public class DocLibNodeInfo {
+
     private RecordRef recordRef;
     private DocLibNodeType nodeType;
     private String displayName;
@@ -18,4 +22,7 @@ public class DocLibNodeInfo {
     private Date created;
     private String modifier;
     private String creator;
+
+    @NotNull
+    private Supplier<ContentData> content;
 }
