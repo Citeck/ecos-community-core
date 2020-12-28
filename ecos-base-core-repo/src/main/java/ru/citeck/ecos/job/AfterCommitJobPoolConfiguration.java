bb7b11457ad540e85232416de0326b96b4c4a222
Roman Makarskiy <Romanchabest55@gmail.com>
2020-12-28 18:24:39 +0300

    ECOSCOM-4122 - Add thread pool for after commit jobs

---
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/job/AfterCommitJobPoolConfiguration.java
@@ -0,0 +1,25 @@
+package ru.citeck.ecos.job;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AfterCommitJobPoolConfiguration {
+
+    @Bean
+    public TaskExecutor afterCommitTaskExecutor() {
+        //TODO: make configurable
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("after-commit-task-Executor-");
+        executor.setAwaitTerminationSeconds(30);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
+}
