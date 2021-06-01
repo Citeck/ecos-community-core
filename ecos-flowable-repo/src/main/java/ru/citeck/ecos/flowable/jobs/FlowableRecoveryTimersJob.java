832ade8b069a2f7d96df7ab446d66f4d8bc02aab
Zaytsev Aleksey <aleksey.zaytsev@citeck.ru>
2021-06-01 16:16:06 +0700

    ECOSCOM-4299 - Flowable. Process with timers does not move, after crash Added service and job

---
+++ ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/jobs/FlowableRecoveryTimersJob.java
@@ -0,0 +1,37 @@
+package ru.citeck.ecos.flowable.jobs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import ru.citeck.ecos.flowable.services.timer.FlowableTimersRestorerService;
+import ru.citeck.ecos.job.AbstractLockedJob;
+
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
+public class FlowableRecoveryTimersJob extends AbstractLockedJob {
+
+    @Override
+    public void executeJob(JobExecutionContext context) {
+        final JobDataMap data = context.getJobDetail().getJobDataMap();
+
+        AuthenticationUtil.runAsSystem(() -> {
+            FlowableTimersRestorerService flowableTimersRestorerService = (FlowableTimersRestorerService) data.get("flowableTimersRestorerService");
+
+            int maxCountJobForProcess = data.getInt("maxCountJobForProcess");
+            int retriesCount = data.getInt("retriesCount");
+            int batchSize = data.getInt("batchSize");
+            @SuppressWarnings("unchecked")
+            Map<String, Set<String>> processActivitiesToChangeStatus = (Map<String, Set<String>>) data.get("processActivitiesToChangeStatus");
+
+            FlowableTimersRestorerService.Config config = new FlowableTimersRestorerService.Config(
+                maxCountJobForProcess, retriesCount, batchSize, processActivitiesToChangeStatus
+            );
+            flowableTimersRestorerService.restore(config);
+
+            return null;
+        });
+    }
+}
