ebfaf4357a411c7d12f42f0f29ba0770edd336ca
Pavel Simonov <pavel.simonov@citeck.ru>
2021-06-21 09:14:39 +0700

    ECOSCOM-4370 - Active tasks is forbidden for user

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/node/DisplayNameService.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/node/DisplayNameService.java
@@ -4,9 +4,10 @@ import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.extensions.surf.util.I18NUtil;
 import org.springframework.stereotype.Service;
+import ru.citeck.ecos.commons.utils.StringUtils;
 
 import java.util.function.Function;
 
@@ -15,11 +16,13 @@ public class DisplayNameService {
 
     public static final QName QNAME = QName.createQName("", "displayNameService");
 
+    private static final String NO_NAME_MSG_KEY = "ecos.service.display-name.no-name";
+
     private final EvaluatorsByAlfNode<String> evaluators;
 
     @Autowired
     public DisplayNameService(ServiceRegistry serviceRegistry) {
-        evaluators = new EvaluatorsByAlfNode<>(serviceRegistry, node -> "");
+        evaluators = new EvaluatorsByAlfNode<>(serviceRegistry, node -> getNoNameMsg());
     }
 
     @NotNull
@@ -30,6 +33,11 @@ public class DisplayNameService {
     @NotNull
     public String getDisplayName(AlfNodeInfo nodeInfo) {
         return evaluators.eval(nodeInfo);
+    }
+
+    private String getNoNameMsg() {
+        String message = I18NUtil.getMessage(NO_NAME_MSG_KEY);
+        return StringUtils.isBlank(message) ? "No name" : message;
     }
 
     public void register(QName nodeType, Function<AlfNodeInfo, String> evaluator) {
