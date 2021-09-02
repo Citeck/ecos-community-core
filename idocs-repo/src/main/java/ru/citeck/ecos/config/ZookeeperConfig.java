5e8d48718188a418d8cf743732680b421ce648cc
Roman.Makarskiy <roman.makarskiy@citeck.ru>
2021-09-02 11:01:28 +0300

    ECOSCOM-3916 - events system

--- idocs-repo/src/main/java/ru/citeck/ecos/config/ZookeeperConfig.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/config/ZookeeperConfig.java
@@ -4,52 +4,54 @@ import ecos.org.apache.curator.RetryPolicy;
 import ecos.org.apache.curator.framework.CuratorFramework;
 import ecos.org.apache.curator.framework.CuratorFrameworkFactory;
 import ecos.org.apache.curator.retry.ExponentialBackoffRetry;
-import ecos.org.apache.curator.retry.RetryForever;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.citeck.ecos.props.EcosPropertiesService;
 import ru.citeck.ecos.zookeeper.EcosZooKeeper;
 
 @Slf4j
 @Configuration
 public class ZookeeperConfig {
 
-    public static final String PROP_HOST_KEY = "ecos.zookeeper.host";
-    public static final String PROP_PORT_KEY = "ecos.zookeeper.port";
-    public static final String PROP_USERNAME_KEY = "ecos.zookeeper.username";
-    public static final String PROP_PASSWORD_KEY = "ecos.zookeeper.password";
+    @Value("${ecos.zookeeper.host}")
+    private String host;
 
-    @Autowired
-    private EcosPropertiesService properties;
+    @Value("${ecos.zookeeper.port}")
+    private int port;
+
+    @Value("${ecos.zookeeper.namespace}")
+    private String namespace;
+
+    @Value("${ecos.zookeeper.curator.retry-policy.base-sleep}")
+    private int baseSleepTime;
+
+    @Value("${ecos.zookeeper.curator.retry-policy.max-retries}")
+    private int maxRetries;
 
     @Bean
-    public EcosZooKeeper ecosZookeeper() {
+    public EcosZooKeeper ecosZookeeper(CuratorFramework curatorFramework) {
+        return new EcosZooKeeper(curatorFramework).withNamespace(namespace);
+    }
 
-        log.info("EcosZookeeper init");
+    @Bean
+    public CuratorFramework curatorFramework() {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(baseSleepTime, maxRetries);
+        String connectString = host + ":" + port;
 
-        /*String host = properties.getProperty(PROP_HOST_KEY, String.class);
-        if (StringUtils.isBlank(host)) {
-            throw new IllegalStateException("'" + PROP_HOST_KEY + "' is undefined");
-        }*/
-        //Integer port = properties.getProperty(PROP_PORT_KEY, Integer.class, () -> 2181);
-
-        RetryPolicy retryPolicy = new ExponentialBackoffRetry(5_000, 10);
-        String connectString = "localhost" + ":" + "2181";
-
-        log.info("=========================================================");
-        log.info("Connect to Zookeeper with URL: " + connectString);
-        log.info("Zookeeper is a mandatory dependency for Alfresco!");
+        log.info("================Ecos Zookeeper Init======================");
+        log.info("Connect to Zookeeper with params");
+        log.info("URL: " + connectString);
+        log.info("namespace: " + namespace);
+        log.info("baseSleepTime: " + baseSleepTime);
+        log.info("maxRetries: " + maxRetries);
         log.info("Startup will be stopped until Zookeeper will be available");
         log.info("=========================================================");
 
-        CuratorFramework client = CuratorFrameworkFactory
-            .newClient(connectString, retryPolicy);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(connectString, retryPolicy);
 
         client.start();
 
-        return new EcosZooKeeper(client).withNamespace("ecos");
+        return client;
     }
 }
