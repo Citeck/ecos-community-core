81e8732eb4ab0656f1547a049f1997927a899741
Pavel Tkachenko <pavel.tkachenko@citeck.ru>
2021-09-06 10:04:40 +0300

    ECOSCOM-4463 - ru.citeck.ecos.content.deploy.ContentDeployer: add deployIfNotExistsOnly property

--- idocs-repo/src/main/java/ru/citeck/ecos/content/deploy/ContentDeployer.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/content/deploy/ContentDeployer.java
@@ -13,7 +13,6 @@ import org.alfresco.util.ParameterCheck;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.BeanNameAware;
@@ -29,7 +28,8 @@ import ru.citeck.ecos.content.metadata.MetadataExtractor;
 import ru.citeck.ecos.model.EcosContentModel;
 
 import javax.annotation.PostConstruct;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.Serializable;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -188,15 +188,12 @@ public class ContentDeployer<T> extends AbstractLifecycleBean implements BeanNam
 
         ByteArrayInputStream inputBytesStream = new ByteArrayInputStream(info.data);
 
-        MutableBoolean skip = new MutableBoolean(false);
-
         Optional<? extends ContentData<?>> data = repoContentDAO.getFirstContentData(info.keys, false);
-        NodeRef contentNode = data.map(contentData -> {
-            skip.setValue(deployIfNotExistsOnly);
-            return contentData.getNodeRef();
-        }).orElseGet(() -> repoContentDAO.createNode(info.metadata));
+        NodeRef contentNode = data.map(ContentData::getNodeRef).orElse(null);
 
-        if (skip.isTrue()) {
+        if (contentNode == null) {
+            contentNode = repoContentDAO.createNode(info.metadata);
+        } else if (deployIfNotExistsOnly) {
             return;
         }
 
