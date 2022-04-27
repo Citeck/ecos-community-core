0fe4044ed24ccb94045922ab88f87891f3d07a6b
Pavel Simonov <pavel.simonov@citeck.ru>
2022-04-28 06:53:19 +0700

    ECOSCOM-4665 - Process statistic API
    (cherry picked from commit 9ec21cf29d2dcc412e6ba5a454bd5d4b0fb04654)

--- ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/bpm/DeployBpmProcessPost.java
+++ ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/bpm/DeployBpmProcessPost.java
@@ -2,36 +2,20 @@ package ru.citeck.ecos.flowable.bpm;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.alfresco.model.ContentModel;
-import org.alfresco.service.cmr.repository.ContentReader;
-import org.alfresco.service.cmr.repository.ContentService;
 import org.alfresco.service.cmr.repository.NodeRef;
-import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.namespace.QName;
 import org.alfresco.util.ParameterCheck;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.extensions.webscripts.AbstractWebScript;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 import org.springframework.extensions.webscripts.WebScriptResponse;
-import ru.citeck.ecos.eapps.WorkflowArtifact;
-import ru.citeck.ecos.eapps.WorkflowArtifactHandler;
-import ru.citeck.ecos.model.EcosBpmModel;
-
-import java.io.InputStream;
-import java.io.Serializable;
-import java.util.Map;
+import ru.citeck.ecos.workflow.EcosBpmAppModelUtils;
 
 @RequiredArgsConstructor(onConstructor_={@Autowired})
 public class DeployBpmProcessPost extends AbstractWebScript {
 
     private static final String PARAM_NODE_REF = "nodeRef";
-
-    private final ContentService contentService;
-    private final NodeService nodeService;
-    private final WorkflowArtifactHandler workflowArtifactHandler;
+    private final EcosBpmAppModelUtils ecosBpmAppModelUtils;
 
     @Override
     @SneakyThrows
@@ -41,27 +25,7 @@ public class DeployBpmProcessPost extends AbstractWebScript {
         ParameterCheck.mandatoryString(PARAM_NODE_REF, nodeRefStr);
         nodeRefStr = nodeRefStr.replace("alfresco/@", "");
 
-        NodeRef nodeRef = new NodeRef(nodeRefStr);
-        Map<QName, Serializable> props = nodeService.getProperties(nodeRef);
-
-        String engineId = (String) props.get(EcosBpmModel.PROP_ENGINE);
-        if (StringUtils.isBlank(engineId)) {
-            engineId = "flowable";
-        }
-        String processId = (String) props.get(EcosBpmModel.PROP_PROCESS_ID);
-        if (StringUtils.isBlank(processId)) {
-            throw new RuntimeException("Process ID is empty for nodeRef: " + nodeRefStr);
-        }
-
-        ContentReader reader = contentService.getReader(nodeRef, ContentModel.PROP_CONTENT);
-
-        try (InputStream in = reader.getContentInputStream()) {
-            WorkflowArtifact artifact = new WorkflowArtifact();
-            artifact.setId(engineId + "$" + processId);
-            artifact.setXmlData(IOUtils.toByteArray(in));
-            workflowArtifactHandler.fireWorkflowChanged(artifact);
-            workflowArtifactHandler.deployArtifact(artifact);
-        }
+        ecosBpmAppModelUtils.deployProcess(new NodeRef(nodeRefStr));
 
         res.setStatus(Status.STATUS_OK);
     }
--- idocs-repo/src/main/java/ru/citeck/ecos/workflow/EcosBpmAppModelUtils.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/workflow/EcosBpmAppModelUtils.java
@@ -1,5 +1,6 @@
 package ru.citeck.ecos.workflow;
 
+import kotlin.Unit;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.alfresco.model.ContentModel;
@@ -7,17 +8,25 @@ import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.ContentReader;
 import org.alfresco.service.cmr.repository.ContentService;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.workflow.WorkflowDeployment;
 import org.alfresco.service.cmr.workflow.WorkflowService;
 import org.alfresco.util.ParameterCheck;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.citeck.ecos.commons.json.Json;
 import ru.citeck.ecos.records2.RecordRef;
-import ru.citeck.ecos.records2.RecordsService;
-import ru.citeck.ecos.records2.graphql.meta.annotation.MetaAtt;
+import ru.citeck.ecos.records2.predicate.PredicateService;
+import ru.citeck.ecos.records2.predicate.model.Predicates;
+import ru.citeck.ecos.records3.RecordsService;
+import ru.citeck.ecos.records3.record.atts.schema.annotation.AttName;
+import ru.citeck.ecos.records3.record.dao.query.dto.query.Consistency;
+import ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -33,7 +42,25 @@ public class EcosBpmAppModelUtils {
 
         ParameterCheck.mandatory("nodeRef", nodeRef);
 
-        ProcessDto processDto = recordsService.getMeta(RecordRef.valueOf(nodeRef.toString()), ProcessDto.class);
+        ProcessDto processDto = recordsService.getAtts(RecordRef.valueOf(nodeRef.toString()), ProcessDto.class);
+        if (StringUtils.isBlank(processDto.processId)) {
+            throw new RuntimeException("Process ID is blank. NodeRef: " + nodeRef);
+        }
+        RecordRef existingDeployment = recordsService.queryOne(RecordsQuery.create(b -> {
+            b.withSourceId("alfresco/");
+            b.withQuery(Predicates.and(
+                Predicates.eq("PARENT", nodeRef.toString()),
+                Predicates.eq("ecosbpm:deploymentProcDefVersion", processDto.versionLabel)
+            ));
+            b.withLanguage(PredicateService.LANGUAGE_PREDICATE);
+            b.withConsistency(Consistency.TRANSACTIONAL);
+            return Unit.INSTANCE;
+        }));
+        if (existingDeployment != null) {
+            log.info("Workflow " + nodeRef + " already deployed. DeploymentRef: " + existingDeployment.getId());
+            return;
+        }
+
         String engineId = processDto.engineId;
         if (StringUtils.isBlank(engineId)) {
             engineId = "flowable";
@@ -41,7 +68,22 @@ public class EcosBpmAppModelUtils {
         ContentReader contentReader = contentService.getReader(nodeRef, ContentModel.PROP_CONTENT);
 
         try (InputStream in = contentReader.getReader().getContentInputStream()) {
-            workflowService.deployDefinition(engineId, in, contentReader.getMimetype());
+
+            WorkflowDeployment deployment = workflowService.deployDefinition(engineId, in, contentReader.getMimetype());
+
+            Map<String, Object> deploymentProps = new LinkedHashMap<>();
+            deploymentProps.put("type", "ecosbpm:deploymentInfo");
+            deploymentProps.put("_parent", nodeRef.toString());
+            deploymentProps.put("_parentAtt", "ecosbpm:deployments");
+            deploymentProps.put("ecosbpm:deploymentProcDefId", processDto.processId);
+            deploymentProps.put("ecosbpm:deploymentProcDefVersion", processDto.versionLabel);
+            deploymentProps.put("ecosbpm:deploymentEngine", engineId);
+            deploymentProps.put("ecosbpm:deploymentVersion", deployment.getDefinition().getVersion());
+
+            RecordRef deploymentRef = recordsService.create("alfresco/", deploymentProps);
+            log.info("Deployment was created: " + deploymentRef.getId()
+                + " with props: " + Json.getMapper().toString(deploymentProps));
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -62,7 +104,11 @@ public class EcosBpmAppModelUtils {
 
     @Data
     public static class ProcessDto {
-        @MetaAtt("ecosbpm:engine?str")
+        @AttName("ecosbpm:engine?str")
         private String engineId;
+        @AttName("ecosbpm:processId?str")
+        private String processId;
+        @AttName("cm:versionLabel!'1.0'")
+        private String versionLabel;
     }
 }
