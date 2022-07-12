b98fb49d9e4f56172148f9f1dd14c11e50b56696
Elena Shushunova <elena.shushunova@citeck.ru>
2022-07-12 13:43:57 +0500

    ECOSENT-2346 - Error on task completion event

--- idocs-repo/src/main/java/ru/citeck/ecos/workflow/tasks/EcosTaskService.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/workflow/tasks/EcosTaskService.java
@@ -1,13 +1,19 @@
 package ru.citeck.ecos.workflow.tasks;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import kotlin.Unit;
+import lombok.Data;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.transaction.TransactionService;
 import org.alfresco.util.ParameterCheck;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.mozilla.javascript.JavaScriptException;
 import org.mozilla.javascript.NativeJavaObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +21,25 @@ import org.springframework.extensions.surf.util.I18NUtil;
 import org.springframework.stereotype.Service;
 import ru.citeck.ecos.comment.CommentTag;
 import ru.citeck.ecos.comment.EcosCommentTagService;
+import ru.citeck.ecos.commons.data.DataValue;
 import ru.citeck.ecos.commons.data.MLText;
+import ru.citeck.ecos.context.lib.auth.AuthContext;
+import ru.citeck.ecos.events2.EventsService;
+import ru.citeck.ecos.events2.emitter.EventsEmitter;
 import ru.citeck.ecos.locks.LockUtils;
 import ru.citeck.ecos.model.CiteckWorkflowModel;
 import ru.citeck.ecos.props.EcosPropertiesService;
 import ru.citeck.ecos.records2.RecordRef;
+import ru.citeck.ecos.records3.record.request.RequestContext;
 import ru.citeck.ecos.workflow.owner.OwnerAction;
 import ru.citeck.ecos.workflow.owner.OwnerService;
 
+import javax.annotation.PostConstruct;
+import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Service
@@ -53,6 +68,24 @@ public class EcosTaskService {
 
     @Autowired
     private EcosCommentTagService commentTagService;
+
+    @Autowired
+    private EventsService eventsService;
+
+    @Autowired
+    private TransactionService transactionService;
+
+    private EventsEmitter<EcosTaskErrorEvent> taskCompletedWithErrorEmitter;
+
+    @PostConstruct
+    public void init() {
+        taskCompletedWithErrorEmitter = eventsService.getEmitter(builder -> {
+            builder.setSource("ecos-flowable");
+            builder.setEventClass(EcosTaskErrorEvent.class);
+            builder.setEventType("bpmn-user-task-complete-error");
+            return Unit.INSTANCE;
+        });
+    }
 
     public void endTask(String taskId, Map<String, Object> variables) {
         endTask(taskId, null, variables, null);
@@ -127,8 +160,68 @@ public class EcosTaskService {
                     log.error("Cannot release task with id: " + taskId, changeOwnerException);
                 }
             }
+
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            executorService.execute(() -> emitEvent(taskInfo, exception));
+            executorService.shutdown();
+
             unwrapJsExceptionAndThrow(exception);
         }
+    }
+
+    private void emitEvent(TaskInfo taskInfo, RuntimeException exception) {
+        AuthContext.runAsSystemJ(() ->
+            RequestContext.doWithTxnJ(() -> {
+                RetryingTransactionHelper helper = transactionService.getRetryingTransactionHelper();
+                helper.doInTransaction(() -> {
+                        EcosTaskErrorEvent taskEvent = new EcosTaskErrorEvent();
+                        taskEvent.setErrorMessage(exception.getMessage());
+                        taskEvent.setErrorStackTrace(ExceptionUtils.getStackTrace(exception));
+                        taskEvent.setAssignee(taskInfo.getAssignee());
+                        taskEvent.setTaskId(taskInfo.getId());
+                        taskEvent.setName(new MLText(taskInfo.getTitle()));
+                        taskEvent.setEngine("flowable");
+                        if (taskInfo.getWorkflow() != null && taskInfo.getWorkflow().getDefinition() != null) {
+                            String fullDefId = taskInfo.getWorkflow().getDefinition().getId();
+                            String processDefId = fullDefId.substring(fullDefId.indexOf("$") + 1);
+                            taskEvent.setProcDefId(processDefId);
+                            String versionValue = taskInfo.getWorkflow().getDefinition().getVersion();
+                            if (StringUtils.isNotBlank(versionValue)) {
+                                try {
+                                    taskEvent.setProcDeploymentVersion(Integer.valueOf(versionValue).intValue());
+                                } catch (IllegalArgumentException e) {
+                                }
+                            }
+                        }
+                        taskEvent.setCreated(toInstantOrNull(taskInfo.getAttribute("cm_created")));
+                        taskEvent.setDueDate(toInstantOrNull(taskInfo.getAttribute("bpm_dueDate")));
+                        taskEvent.setDescription(taskInfo.getDescription());
+                        taskEvent.setPriority(toIntegerOrNull(taskInfo.getAttribute("bpm_priority")));
+                        String taskTitle = toStringOrNull(taskInfo.getAttribute("cwf_taskTitle"));
+                        if (StringUtils.isBlank(taskTitle)) {
+                            taskTitle = taskInfo.getTitle();
+                        }
+                        taskEvent.setName(new MLText(taskTitle));
+                        taskEvent.setDocument(taskInfo.getDocument());
+                        taskEvent.setComment(toStringOrNull(taskInfo.getAttribute("comment")));
+                        taskEvent.setOutcome(toStringOrNull(taskInfo.getAttribute("outcome")));
+                        taskEvent.setCompletedViaMail(toBoolean(taskInfo.getAttribute("isCompletedViaMail")));
+                        Object formInfo = taskInfo.getAttribute("_formInfo");
+                        if (formInfo instanceof ObjectNode) {
+                            Object outcomeName = ((ObjectNode) formInfo).get("submitName");
+                            if (outcomeName instanceof ObjectNode) {
+                                Map<Locale, String> name =
+                                    DataValue.create(outcomeName).asMap(Locale.class, String.class);
+                                taskEvent.setOutcomeName(new MLText(name));
+                            }
+                        }
+
+                        taskCompletedWithErrorEmitter.emit(taskEvent);
+                        return null;
+                    },
+                    true,
+                    true);
+            }));
     }
 
     private void addCommentToDocument(RecordRef taskDocument, org.alfresco.service.cmr.repository.MLText taskMlTitle,
@@ -219,10 +312,43 @@ public class EcosTaskService {
         return taskService;
     }
 
+    private static Instant toInstantOrNull(Object date) {
+        if (date == null || !(date instanceof Date)) {
+            return null;
+        }
+        return ((Date) date).toInstant();
+    }
+
+    private static Integer toIntegerOrNull(Object value) {
+        if (value == null || !(value instanceof Integer)) {
+            return null;
+        }
+        return (Integer) value;
+    }
+
+    private static boolean toBoolean(Object value) {
+        if (value == null) {
+            return false;
+        }
+        return Boolean.valueOf(value.toString());
+    }
+
+    private static String toStringOrNull(Object value) {
+        if (value == null) {
+            return null;
+        }
+        if (value instanceof String) {
+            return (String) value;
+        }
+        return String.valueOf(value);
+    }
+
     private static class TaskId {
 
-        @Getter private final String engine;
-        @Getter private final String localId;
+        @Getter
+        private final String engine;
+        @Getter
+        private final String localId;
 
         TaskId(String taskId) {
             int delimIdx = taskId.indexOf('$');
@@ -241,5 +367,29 @@ public class EcosTaskService {
     @Autowired
     public void setLockUtils(LockUtils lockUtils) {
         this.lockUtils = lockUtils;
+    }
+
+    @Data
+    private static class EcosTaskErrorEvent {
+        private String taskId;
+        private String engine;
+        private String assignee;
+        private String procDefId;
+        private int procDeploymentVersion;
+        private String procInstanceId;
+        private String elementDefId;
+        private Instant created;
+        private Instant dueDate;
+        private String description;
+        private int priority;
+        private String executionId;
+        private MLText name;
+        private String comment;
+        private String outcome;
+        private MLText outcomeName;
+        private RecordRef document;
+        private boolean isCompletedViaMail;
+        private String errorMessage;
+        private String errorStackTrace;
     }
 }
