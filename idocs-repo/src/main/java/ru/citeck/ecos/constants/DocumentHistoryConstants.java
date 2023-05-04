86febc39ca4fe9d9a5b9f5cffac421d4e4c2458b
ilya.kaygorodov <ilya.kaygorodov@citeck.ru>
2023-05-04 16:16:01 +0300

    Revert "ECOSCOM-4626 - History-Service: Event History section changes"
    This reverts commit 1ae504d1b6b6d2d672c0e663817805b7aa03bb8b.

--- idocs-repo/src/main/java/ru/citeck/ecos/constants/DocumentHistoryConstants.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/constants/DocumentHistoryConstants.java
@@ -11,7 +11,7 @@ public class DocumentHistoryConstants {
     public static final AbstractMap.SimpleEntry<String, String> EVENT_INITIATOR = new AbstractMap.SimpleEntry<>("event:initiator", "username");
     public static final AbstractMap.SimpleEntry<String, String> DOCUMENT_ID = new AbstractMap.SimpleEntry<>("event:documentId", "documentId");
     public static final AbstractMap.SimpleEntry<String, String> DOCUMENT_DATE = new AbstractMap.SimpleEntry<>("event:date", "creationTime");
-    public static final AbstractMap.SimpleEntry<String, String> DOCUMENT_VERSION = new AbstractMap.SimpleEntry<>("event:documentVersion", "documentVersion");
+    public static final AbstractMap.SimpleEntry<String, String> DOCUMENT_VERSION = new AbstractMap.SimpleEntry<>("event:documentVersion", "version");
     public static final AbstractMap.SimpleEntry<String, String> COMMENTS = new AbstractMap.SimpleEntry<>("event:taskComment", "comments");
     public static final AbstractMap.SimpleEntry<String, String> EVENT_TYPE = new AbstractMap.SimpleEntry<>("event:name", "eventType");
     public static final AbstractMap.SimpleEntry<String, String> TASK_ROLE = new AbstractMap.SimpleEntry<>("event:taskRole", "taskRole");
